92a6660cc3dad898d758f92e05a646128e425978
Eywon25 <aaronjonalberto@gmail.com>
2023-05-24 17:00:30 +0800

    latest

---
+++ script.js
@@ -0,0 +1,17 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function selectionMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function selection(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
